060eac9e5467c6fcb4b9a2d4f6fa552cbf012efe
naderscode <naderscode@gmail.com>
2018-12-18 15:58:43 -0500

    refactor JS code

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,33 @@
 var main = function(){
 	"use strict"
 
+
+function makeActive(i){
+
+	var tabSelector = ".tabs a:nth-child(" + i + ") span";
+		$(".tabs span").removeClass("active");
+		$(tabSelector).addClass("active");
+		$("main .content").empty();
+		
+
+};
+
+$(".tabs a:nth-child(1)").on("click", function(){
+		makeActive(1);
+		return false;
+
+	});
+
+
+	/*
+
 	$(".tabs a:nth-child(1)").on("click", function(){
 		$(".tabs span").removeClass("active");
 		$(".tabs a:nth-child(1) span").addClass("active");
 		$("main .content").empty();
 		return false;
 
-	})
+	});
 
 	$(".tabs a:nth-child(2)").on("click", function(){
 		$(".tabs span").removeClass("active");
@@ -15,7 +35,7 @@ var main = function(){
 		$("main .content").empty();
 		return false;
 
-	})
+	});
 
 	$(".tabs a:nth-child(3)").on("click", function(){
 		$(".tabs span").removeClass("active");
@@ -23,7 +43,8 @@ var main = function(){
 		$("main .content").empty();
 		return false;
 
-	})
+	});
+*/
 
 };
 
