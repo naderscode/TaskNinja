2d969c65fc9a982d7cf85bfd182c4d87f1a90b66
naderscode <naderscode@gmail.com>
2018-12-18 15:11:32 -0500

    create tab functionality

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,29 @@
 var main = function(){
 	"use strict"
 
-	
+	$(".tabs a:nth-child(1)").on("click", function(){
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(1) span").addClass("active");
+		$("main .content").empty();
+		return false;
+
+	})
+
+	$(".tabs a:nth-child(2)").on("click", function(){
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(2) span").addClass("active");
+		$("main .content").empty();
+		return false;
+
+	})
+
+	$(".tabs a:nth-child(3)").on("click", function(){
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(3) span").addClass("active");
+		$("main .content").empty();
+		return false;
+
+	})
 
 };
 
