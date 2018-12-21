abd25fa74444f2627509b842180df3ec30b07868
naderscode <naderscode@gmail.com>
2018-12-20 21:17:02 -0500

    crerate tags data structure and input field

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,170 +1,129 @@
-var main = function(toDoObjects){ /* pass toDoObjects as a parameter so that main has access to the todos */
-	"use strict";
+var main = function (toDoObjects) {
+    "use strict";
 
-var toDos = toDoObjects.map(function(toDo){
-	return toDo.description;
-});
+    var toDos = toDoObjects.map(function (toDo) {
+          // return the description
+          // of this toDoObject
+          return toDo.description;
+    });
 
-//using jQuery forEach loop.
-$(".tabs a span").toArray().forEach(function(element){
-	
-	//create a click handler for element
-	var $element = $(element);
-	$element.on("click", function(){
+    $(".tabs a span").toArray().forEach(function (element) {
+        var $element = $(element);
 
-		
-		var $content, $input, $button;
+        // create a click handler for this element
+        $element.on("click", function () {
+            var $content,
+                $input,
+                $button;
+                
 
-		$(".tabs span").removeClass("active");
-		$element.addClass("active");
-		$("main .content").empty();
-		
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
 
-		if($element.parent().is(":nth-child(1)")){
-			//newest todos first
-			$content = $("<ul>");
-			for (var i = toDos.length-1; i >= 0; i--){
-				$content.append($("<li>").text(toDos[i]));
-				
-			}
+            if ($element.parent().is(":nth-child(1)")) {
+                $content = $("<ul>");
+                for (var i = toDos.length-1; i >= 0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+            } else if ($element.parent().is(":nth-child(2)")) {
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
 
-		} else if($element.parent().is(":nth-child(2)")){
-				//oldest todos first
-				$content = $("<ul>");
-				toDos.forEach(function(todo){
-					$content.append($("<li>").text(todo));
-					
-				});	
+            } else if ($element.parent().is(":nth-child(3)")) {
+                //create an empty tags array
+                var tags = [];
 
-		} else if($element.parent().is(":nth-child(3)")){
-				//oldest todos first
-				console.log("tags");
+                //iterate over all todos
+                toDoObjects.forEach(function (toDo) {
+                    
+                    //iterate over each tag in this todo
+                    toDo.tags.forEach(function (tag) {
+                        
+                    	//make sure tag does not exist in the tag array
+                        if (tags.indexOf(tag) === -1) {
+                            tags.push(tag);
+                        }
+                    });
+                });
+                console.log(tags);
 
-				var organizedByTag = [
-					{
-						"tagName": "shopping",
-						"toDos": ["Buy Groceries", "Buy Gifts"]
-					},
-					{
-						"tagName": "personal care",
-						"toDos": ["Hair cut", "Do a Massage", "Gym Session"]
-					},
-					{
-						"tagName": "work",
-						"toDos": ["Meet with client", "Design new app"]
-					},
-					{
-						"tagName": "meetings",
-						"toDos": ["Meet with client"]
-					},
-					{
-						"tagName": "design",
-						"toDos": ["Design new app"]
-					},
-					{
-						"tagName": "auto",
-						"toDos": ["Oil change", "Renew Auto Insurance"]
-					}
+                var tagObjects = tags.map(function (tag) {
+                    
+                	//create an array for todos that have tags
+                    var toDosWithTag = [];
 
-				]
+                    //find all todos that have that tag
+                    toDoObjects.forEach(function (toDo) {
+                        //make sure the result of indexOf is NOT equal to -1
+                        if (toDo.tags.indexOf(tag) !== -1) {
+                            toDosWithTag.push(toDo.description);
+                        }
+                    });
 
-			organizedByTag.forEach(function(tag){
-				var $tagName = $("<h3>").text(tag.tagName);
-				$content =$("<ul>");
+                    //map each tag to an object that contain the name of the tag
+                    //and an array of todo descriptions
+                    return { "name": tag, "toDos": toDosWithTag };
+                });
 
-				tag.toDos.forEach(function(description){
-					var $li = $("<li>").text(description);
-					$content.append($li);
-				});
+                console.log(tagObjects);
+
+                tagObjects.forEach(function (tag) {
+                    var $tagName = $("<h3>").text(tag.name),
+                        $content = $("<ul>");
 
 
-				$("main .content").append($tagName).append($content);
+                    tag.toDos.forEach(function (description) {
+                        var $li = $("<li>").text(description);
+                        $content.append($li);
+                    });
 
+                    $("main .content").append($tagName);
+                    $("main .content").append($content);
+                });
 
-			});
+            } else if ($element.parent().is(":nth-child(4)")) {
+                var $input = $("<input>").addClass("description"),
+                    $inputLabel = $("<p>").text("Description: "),
+                    $tagInput = $("<input>").addClass("tags"),
+                    $tagLabel = $("<p>").text("Tags (seperate by commas): "),
+                    $button = $("<button>").text("+");
 
-				
+                $button.on("click", function () {
+                    var description = $input.val(),
+                        tags = $tagInput.val().split(",");
+                                 
+                    toDoObjects.push({"description":description, "tags":tags});
 
-		} else if($element.parent().is(":nth-child(4)")){
-				// input a new todo
-				$input = $("<input>");
-				$button = $("<button>").text("+");
+                    // update toDos
+                    toDos = toDoObjects.map(function (toDo) {
+                        return toDo.description;
+                    });
 
+                    $input.val("");
+                    $tagInput.val("");
+                });
 
-				$button.on("click", function(){
-					if ($input.val() !== 0){
-						toDos.push($input.val());
-						$input.val("");
-					}
+                $content = $("<div>").append($inputLabel)
+                                     .append($input)
+                                     .append($tagLabel)
+                                     .append($tagInput)
+                                     .append($button);
+            }
 
-				});
+            $("main .content").append($content);
 
-				$content = $("<div>");
-				$content.append($input).append($button);
+            return false;
+        });
+    });
 
-		} 
-
-		$("main .content").append($content);
-
-		return false;
-
-	});
-});
-
-/*
-//Refactoring using a for loop
-for (var i = 1 ; i <= 3; i++){
-
-	var tabSelector = ".tabs a:nth-child(" + i + ") span";
-
-	$(tabSelector).on("click", function(event){
-		$(".tabs span").removeClass("active");
-		$(event.target).addClass("active");
-		$("main .content").empty();
-		return false;
-
-	});
-}
-*/
-/*
-//Refactoring using a function
-function makeActive(i){
-
-	var tabSelector = ".tabs a:nth-child(" + i + ") span";
-		$(".tabs span").removeClass("active");
-		$(tabSelector).addClass("active");
-		$("main .content").empty();
-		
-
+    $(".tabs a:first-child span").trigger("click");
 };
 
-$(".tabs a:nth-child(1)").on("click", function(){
-		makeActive(1);
-		return false;
-
-});
-
-$(".tabs a:nth-child(2)").on("click", function(){
-		makeActive(2);
-		return false;
-
-});
-
-$(".tabs a:nth-child(3)").on("click", function(){
-		makeActive(3);
-		return false;
-});
-	
-*/
-$(".tabs a:first-child span").trigger("click");
-
-};
-
-//add anonymous function to document.ready that calls getJSON
-//and then calls main with the result
-$(document).ready(function(){
-	$.getJSON("todos.json", function(toDoObjects){
-		//call main with toDoObjects as an argument
-		main(toDoObjects);
-	})
+$(document).ready(function () {
+    $.getJSON("todos.json", function (toDoObjects) {
+        main(toDoObjects);
+    });
 });
